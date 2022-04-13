bafa82f23c5b10a121a73b78ad0cbe657219206f
cygsd <cygsd@163.com>
2022-04-14 00:47:23 +0800

    测试sqlite

--- Window_JiPu.xaml.cs
+++ Window_JiPu.xaml.cs
@@ -20,9 +20,32 @@ namespace Chess
     /// </summary>
     public partial class Window_JiPu : Window
     {
+        System.Data.DataSet ds = new System.Data.DataSet();
         public Window_JiPu()
         {
             InitializeComponent();
+        }
+
+        private void WindowJiPu_Load(object sender, RoutedEventArgs e)
+        {
+            System.Data.SQLite.SQLiteConnection conn = new SQLiteConnection("data source=E:/source/repos/Chess/DB/KaiJuKu.db");
+            conn.Open();
+            
+            System.Data.SQLite.SQLiteCommand comm = conn.CreateCommand();
+            comm.CommandText = "select * from mybook";
+            
+            SQLiteDataAdapter adapter = new SQLiteDataAdapter(comm);
+            _ = adapter.Fill(ds);
+            //Console.WriteLine(ds.Tables.ToString());
+            lv.Items.Add(ds);
+            text1.Text = ds.Tables.ToString();
+
+
+        }
+
+        private void lv_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
         }
     }
 }
