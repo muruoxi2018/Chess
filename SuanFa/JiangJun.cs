e5f7c742b896452d6e039dcff5f5ed385b73f040
cygsd <cygsd@163.com>
2022-04-21 00:18:43 +0800

    将军检查模块

---
+++ SuanFa/JiangJun.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chess.SuanFa
+{
+    class JiangJun
+    {
+        public static int isJiangJun(int qizi)
+        {
+            MoveCheck.GetPathPoints(qizi);
+            int x = GlobalValue.QiZiArray[0].Col;
+            int y = GlobalValue.QiZiArray[0].Row;
+            if (MoveCheck.PathBool[x,y]== true) return 0;
+            x = GlobalValue.QiZiArray[16].Col;
+            y = GlobalValue.QiZiArray[16].Row;
+            if (MoveCheck.PathBool[x, y] == true) return 16;
+            return -1;
+        }
+    }
+}
