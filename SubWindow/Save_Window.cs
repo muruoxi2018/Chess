36b50878525f11eb507c7f35850f7d13d58b1ad6
cygsd <cygsd@163.com>
2022-05-01 00:36:25 +0800

    提交遗漏文件

---
+++ SubWindow/Save_Window.cs
@@ -0,0 +1,52 @@
+﻿using System.Windows;
+using Newtonsoft.Json;
+using Chess.SuanFa;
+using Chess.DataClass;
+using Chess.OpenSource;
+using System.Data;
+
+namespace Chess.SubWindow
+{
+    /// <summary>
+    /// Save_Window.xaml 的交互逻辑
+    /// 保存棋谱窗口
+    /// </summary>
+    public partial class Save_Window : Window
+    {
+        public Save_Window()
+        {
+            InitializeComponent();
+            qipustr.Text = Qipu.CnToString();
+            DataTable dt = SqliteHelper.Select("mybook", "author");
+            dt = new DataView(dt).ToTable(true, "author"); // 去除重复数据
+            author.DisplayMemberPath = "author";
+            author.ItemsSource = dt.DefaultView;
+
+            dt = SqliteHelper.Select("mybook", "type");
+            dt = new DataView(dt).ToTable(true, "type"); // 去除重复数据
+            type.DisplayMemberPath = "type";
+            type.ItemsSource = dt.DefaultView;
+
+            dt = SqliteHelper.Select("mybook", "title");
+            dt = new DataView(dt).ToTable(true, "title"); // 去除重复数据
+            title.DisplayMemberPath = "title";
+            title.ItemsSource = dt.DefaultView;
+
+        }
+
+        private void SaveButtonClick(object sender, RoutedEventArgs e)
+        {
+            QiPuBook book = new();
+            book.author = author.Text;
+            book.date = date.DisplayDate;
+            book.type = type.Text;
+            book.title = title.Text;
+            book.video = videoLink.Text;
+            book.memo = memoText.Text;
+            book.record = Qipu.CnToString();
+            book.jsonrecord = JsonConvert.SerializeObject(Qipu.QiPuList);
+            _ = SqliteHelper.Insert("mybook", book.getDictionary());
+            Close();
+        }
+    }
+}
---
+++ SubWindow/Window_QiPu.cs
@@ -0,0 +1,72 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using Newtonsoft.Json;
+using Chess.SuanFa;
+using System.Data;
+using System.Collections.ObjectModel;
+
+namespace Chess
+{
+    /// <summary>
+    /// Window1.xaml 的交互逻辑
+    /// </summary>
+    public partial class Window_QiPu : Window
+    {
+        public Window_QiPu()
+        {
+            InitializeComponent();
+        }
+        /// <summary>
+        /// 窗口打开时，显示棋谱库列表，以及走棋记录
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void WindowQiPu_Load(object sender, RoutedEventArgs e)
+        {
+            DataTable sr = OpenSource.SqliteHelper.Select("mybook", "rowid,*");
+            datagrid.ItemsSource = sr.DefaultView;
+        }
+
+        public void QipuListRefresh(object sender, RoutedEventArgs e)
+        {
+            DataTable sr = OpenSource.SqliteHelper.Select("mybook", "rowid,*");
+            datagrid.ItemsSource = sr.DefaultView;
+        }
+
+        private void OnMouseLeftButtonUP(object sender, MouseButtonEventArgs e)
+        {
+            string jsonstr = ((DataRowView)datagrid.SelectedItem).Row["jsonrecord"].ToString(); // 获得点击行的数据
+            jsontext.Text = jsonstr;
+            TreeViewItem tree = new();
+            tree.Header = "root";
+            tree.IsExpanded = true;
+            if (jsonstr.Length > 5)
+            {
+                ObservableCollection<Qipu.QPStep> ql = JsonConvert.DeserializeObject<ObservableCollection<Qipu.QPStep>>(jsonstr);  // 反序列化为对象
+                foreach (Qipu.QPStep qp in ql)
+                {
+                    _ = tree.Items.Add(qp.ToTreeNode());
+                }
+            }
+            jsonTree.Items.Clear();
+            _ = jsonTree.Items.Add(tree);
+            memostr.Text = ((DataRowView)datagrid.SelectedItem).Row["memo"].ToString();
+        }
+
+        private void OnMouseRightButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            menu.IsOpen = true;
+        }
+
+        private void DeleteRowData(object sender, RoutedEventArgs e)
+        {
+            if (datagrid.SelectedIndex > -1)
+            {
+                string rowid = ((DataRowView)datagrid.SelectedItem).Row["rowid"].ToString();
+                _ = OpenSource.SqliteHelper.Delete("mybook", "rowid=" + rowid);
+                QipuListRefresh(sender, e);
+            }
+        }
+    }
+}
