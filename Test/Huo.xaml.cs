6ea1ecc52dac99b6445906e1a0b88dbe8bc30e9a
cygsd <cygsd@163.com>
2022-09-22 23:05:39 +0800

    主窗口XMAL的x:Class="Chess.MainWindow"跑到其他位置，导致后台代码找不到控件名称。放到第一行就解决了。

--- Test/Huo.xaml.cs
+++ Test/Huo.xaml.cs
@@ -1,17 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Chess.Test
 {
@@ -39,20 +28,12 @@ namespace Chess.Test
             { 608, -5 } };
         public Huo()
         {
-            InitializeComponent();
             System.Windows.Media.CompositionTarget.Rendering += DongHua; // 按每秒60帧速率调用
-
         }
-
-        private void OnLoaded(object sender, RoutedEventArgs e)
-        {
-
-        }
-
         protected void DongHua(object Sender, EventArgs e)
         {
             rate++;
-            if (rate == 12) // 降低帧率
+            if (rate == 20) // 降低帧率
             {
                 rate = 0;
                 index++;
